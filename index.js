255a96b45e2f278fc0b77fdd65e7917bade491cd
Tiffany Owens <owenst92@gmail.com>
2022-03-09 20:08:28 -0500

    made changes to index.js

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
-    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
-}
-console.log(answer)
-
-for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
-    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[0]))
-    monthlyExpenses = monthlyExpenses + answer
-}
-console.log(answer)
-
-for (let i = 0; i < annualExpenseQuestions.lenght; i++) {
-    let answer = parseFloat(window.prompt(annualExpenseQuestions[0]))
-    annualExpenses = annualExpenses + answer
 }
 console.log(answer)
